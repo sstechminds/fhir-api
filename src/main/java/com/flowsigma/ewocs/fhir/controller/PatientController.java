102fb65e5cfcadccad55d5dbdfba484600bd86a5
rvangala <rvangala@gmail.com>
2020-10-11 15:03:32 -0500

    Add resp type

--- src/main/java/com/flowsigma/ewocs/fhir/controller/PatientController.java
+++ src/main/java/com/flowsigma/ewocs/fhir/controller/PatientController.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,53 +27,49 @@ class PatientController {
 		this.om = new ObjectMapper();
 	}
 
-	@GetMapping("/patient")
-	ResponseEntity<List<Map<String, String>>> getPatients() {
+	@GetMapping(value = "/patient", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<Map<String, String>>> getPatients() {
 		List<Map<String, String>> patients = fhirPatientService.getPatients("Patient");
 
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-		return new ResponseEntity<>(patients, responseHeaders, HttpStatus.OK);
+		return new ResponseEntity<>(patients, HttpStatus.OK);
 	}
 
-	@GetMapping("/patient/{patientId}/condition")
-	ResponseEntity<JSONArray> getCondition(@PathVariable String patientId) {
+	@GetMapping(value = "/patient/{patientId}/condition", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<JSONArray> getCondition(@PathVariable String patientId) {
 		JSONArray conditions = fhirPatientService.getPatientCondition("Condition?patient=" + patientId);
 
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-		return new ResponseEntity<>(conditions, responseHeaders, HttpStatus.OK);
+		return new ResponseEntity<>(conditions, HttpStatus.OK);
 	}
 
-	@GetMapping("/imagingstudy/{studyId}")
-	ResponseEntity<String> getPatientImageStudy(@PathVariable String studyId) {
+	@GetMapping(value = "/imagingstudy/{studyId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<String> getPatientImageStudy(@PathVariable String studyId) {
 		String imagingStudy= fhirPatientService.getImagingStudy("ImagingStudy?_id=" + studyId);
 
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-		return new ResponseEntity<>(imagingStudy, responseHeaders, HttpStatus.OK);
+		return new ResponseEntity<>(imagingStudy, HttpStatus.OK);
 	}
 
-	@GetMapping("/patient/{patientId}/diagnosticreport")
-	ResponseEntity<List<String>> getPatientReports(@PathVariable String patientId) {
+	@GetMapping(value = "/patient/{patientId}/diagnosticreport", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<String>> getPatientReports(@PathVariable String patientId) {
 		List<String> drs = fhirPatientService.getPatientDiagnosticReports("DiagnosticReport?patient=" + patientId);
 
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-		return new ResponseEntity<>(drs, responseHeaders, HttpStatus.OK);
+		return new ResponseEntity<>(drs, HttpStatus.OK);
 	}
 
-	@GetMapping("/diagnosticreport/{reportId}")
-	ResponseEntity<String> getDiagnosticReport(@PathVariable String reportId) {
+	@GetMapping(value = "/diagnosticreport/{reportId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<String> getDiagnosticReport(@PathVariable String reportId) {
 		String dr = fhirPatientService.getPatientDiagnosticReport("DiagnosticReport/" + reportId);
 
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-		return new ResponseEntity<>(dr, responseHeaders, HttpStatus.OK);
+		return new ResponseEntity<>(dr, HttpStatus.OK);
 	}
 
-	@GetMapping("/patient/{patientId}/diagnosticreport/{reportId}")
-	Collection<Patient> getPatientReport(@PathVariable String patientId, @PathVariable String reportId) {
+	@GetMapping(value = "/patient/{patientId}/diagnosticreport/{reportId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Collection<Patient> getPatientReport(@PathVariable String patientId, @PathVariable String reportId) {
 		throw new UnsupportedOperationException("Looks like this endpoint is unavailable onn SIIM FHIR endpoints.");
 	}
 }
