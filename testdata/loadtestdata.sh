b3f80f747b26ec942a74ab7542bf06c856b5cc15
rvangala <rvangala@gmail.com>
2020-10-27 15:57:29 -0500

    Added payload id in url

--- testdata/loadtestdata.sh
+++ testdata/loadtestdata.sh
@@ -3,7 +3,7 @@
 # https://tools.knowledgewalls.com/jsontostring
 #curl -X POST http://hackathon.siim.org/fhir/Patient -H 'apikey:dd6f7f1d-1586-438f-8d35-ff589a12f4df' -H "Content-Type: application/fhir+json" -d "{ \"resourceType\": \"Patient\" , \"id\": \"post-curl\" , \"name\": [ { \"family\": \"GUMERCINDO\", \"given\": [ \"ALMEIDA\", \"PEREIRA\" ] } ] , \"gender\": \"male\" , \"birthDate\": \"1970-01-21\"}"
 
-curl -X POST http://hackathon.siim.org/fhir/Patient -H 'apikey:dd6f7f1d-1586-438f-8d35-ff589a12f4df' -H "Content-Type: application/fhir+json"  --data @testdata/files/patient.json
+curl -X PUT http://hackathon.siim.org/fhir/Patient/siimravi3 -H 'apikey:dd6f7f1d-1586-438f-8d35-ff589a12f4df' -H "Content-Type: application/fhir+json"  --data @testdata/files/patient.json
 curl -X POST http://hackathon.siim.org/fhir/ServiceRequest -H 'apikey:dd6f7f1d-1586-438f-8d35-ff589a12f4df' -H 'Content-type: application/json+fhir' --data @testdata/files/servicerequest.json
 curl -X POST http://hackathon.siim.org/fhir/Observation -H 'apikey:dd6f7f1d-1586-438f-8d35-ff589a12f4df' -H 'Content-type: application/json+fhir' --data @testdata/files/observation.json
 curl -X POST http://hackathon.siim.org/fhir/DiagnosticReport -H 'apikey:dd6f7f1d-1586-438f-8d35-ff589a12f4df' -H 'Content-type: application/json+fhir' --data @testdata/files/diagnosticreport.json
